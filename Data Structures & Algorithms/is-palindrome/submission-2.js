15955ca568b87caeee3a89660371b7be0f87fa2a
Chris Pannapara <chris12pannapara@gmail.com>
2026-08-05 11:56:39 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length -1;
+
+        while(left < right){
+            while(left < right && !this.alpaNum(s[left])){
+                left++;
+            }
+            while(right > left && !this.alpaNum(s[right])){
+                right--;
+            }
+            if(s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    alpaNum(c){
+        return(
+            (c >= 'A' && c <='Z') || (c >='a' && c<='z') || (c >= '0' && c<='9')
+        );
+    }
+}
